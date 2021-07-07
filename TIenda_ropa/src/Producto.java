c6c68dc8f674d7afe90090ee8418f52719281307
cagomez06 <camiancaste@hotmail.com>
2021-07-06 20:16:35 -0500

    first commit

---
+++ TIenda_ropa/src/Producto.java
@@ -0,0 +1,24 @@
+public class Producto {
+    
+    private String nombre= "";
+    private double precio= 0.0;
+
+    public Producto(){
+
+    }
+    public void mostrar() {
+        System.out.println(nombre + ":"+ precio);
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public Double getPrecio() {
+        return this.precio;
+    }
+}
